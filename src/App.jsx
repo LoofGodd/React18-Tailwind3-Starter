35843807d4307ac7402bf87b6ad0ac367078f8df
LoofGodd <narot752@gmail.com>
2023-11-16 13:04:58 +0700

    react18Tailwind3

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="mx-auto w-fit bg-purple-600 p-4  text-fuchsia-400 hover:bg-blue-500">
+      Hello this is react
+    </div>
+  );
+}
+
+export default App;
